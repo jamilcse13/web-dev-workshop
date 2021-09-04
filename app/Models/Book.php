a2a8cef0fa9c072f6c87e4fbdb518f0587c74bf7
jamil <jamil.cse13@gmail.com>
2021-09-04 18:56:01 +0600

    created Book and Author model with their relationship

---
+++ app/Models/Book.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $table = 'books';
+
+    protected $guarded = ['id'];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+}
